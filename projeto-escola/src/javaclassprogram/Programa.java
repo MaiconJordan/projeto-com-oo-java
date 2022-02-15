9e1fd8aae74dc47fbb1b7f6cbb19868bcf43aeca
Maicon Jordan <49694644+MaiconJordan@users.noreply.github.com>
2022-02-14 21:54:15 -0300

    listas
    subindo listas

---
+++ projeto-escola/src/javaclassprogram/Programa.java
@@ -0,0 +1,61 @@
+package javaclassprogram;
+
+import javax.swing.JOptionPane;
+
+import javaclasses.Aluno;
+import javaclasses.Disciplina;
+
+public class Programa {
+	
+	public static void main (String []args) {
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno");
+		String cpf = JOptionPane.showInputDialog("Qual o CPF do aluno");
+		String idade = JOptionPane.showInputDialog("Qual a idade do aluno");
+		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno");
+		String nomeMae = JOptionPane.showInputDialog("QQual o nome da mãe do aluno");
+		String codMatricula = JOptionPane.showInputDialog("Qual o codido de matricula do aluno");
+		
+		Aluno aluno1 = new Aluno();
+		
+		aluno1.setNome(nome);
+		aluno1.setCpf(cpf);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setNomeMae(nomeMae);
+		aluno1.setCodMatricula(codMatricula);
+		
+		for (int pos = 1; pos <= 4 ; pos++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Qual a o nome da disciplina "+pos+" ?");
+			String notaDisciplina = JOptionPane.showInputDialog("Qual a nota da disciplina "+pos+" ?");
+			
+			Disciplina disciplinas = new Disciplina();
+			
+			disciplinas.setDisciplina(nomeDisciplina);
+			disciplinas.setNota(Double.valueOf(notaDisciplina));
+			
+			aluno1.getDisciplinas().add(disciplinas);
+			
+		}
+		
+		/* comando para remover uma disciplina/nota da lista*/
+		
+		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina" );
+		
+		if(escolha == 0) {
+			
+			String disciplinaRemover = JOptionPane.showInputDialog("Qual disciplina 1, 2, 3 ou 4?");
+			aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - 1);
+			
+		}
+		
+		System.out.println(aluno1);
+		System.out.println("teste2");
+		System.out.println("A média do aluno é " + aluno1.getMediaNota());
+		System.out.println(aluno1.getAprovado2());
+		
+		
+		
+	}
+
+}
